02a2242f19879d812f0aeb5f3ffb9b77a4b4b275
ahuamanpowercode <ahuaman@powercode.pe>
2024-03-25 18:12:01 -0500

    creacion del proyecto y sus respectivas app

--- Academico/views.py
+++ Academico/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Curso
 
 # Create your views here.
 def home(request):
-    return render (request,'gestionCursos.html')
+    cursolista=Curso.objects.all()
+    return render (request,'gestionCursos.html',{'cursos':cursolista})
 
--- universidad/settings.py
+++ universidad/settings.py
@@ -14,6 +14,9 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+STATICFILES_DIRS = [
+    BASE_DIR / 'universidad/static',  # Ruta a tus archivos estáticos
+]
 
 
 # Quick-start development settings - unsuitable for production
@@ -122,3 +125,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_URL = '/static/'
